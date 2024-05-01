b55028f8d55c744e7f05e402dcd5fd432a6abbb7
Syed Shaheer Ali <shaheeraliali@hotmail.com>
2024-05-01 20:55:28 +0500

    fixed showcar gui

--- src/car/ShowCarGUI.java
+++ src/car/ShowCarGUI.java
@@ -12,14 +12,15 @@ import car.Car;
 
 public class ShowCarGUI extends CarShower implements GUIImplementer{
     protected JFrame showcar_frame;
-
+    protected static String[] str;
     private JLabel show_cars;
+
 
     private JPanel panel;
 
     protected JTextArea info_area;
     private JButton back_button;
-
+    private JScrollPane pane;
     public ShowCarGUI() {
         showcar_frame = new JFrame("Available cars");
 
@@ -30,7 +31,7 @@ public class ShowCarGUI extends CarShower implements GUIImplementer{
 
         back_button = new JButton("Back");
 
-
+        pane = new JScrollPane(info_area);
 
     }
 
@@ -39,11 +40,12 @@ public class ShowCarGUI extends CarShower implements GUIImplementer{
         show_cars.setForeground(Color.orange);
         show_cars.setFont(new Font("Serif", Font.ITALIC, 35));
 
-
-        info_area.setBounds(0, 80, 700, 400);
-        info_area.setBackground(new Color(34, 41, 48));
-        info_area.setForeground(Color.orange);
-        info_area.setFont(new Font("SansSerif", Font.PLAIN, 18));
+        pane.setBounds(0, 80, 700, 400);
+//
+//        info_area.setBounds(0, 80, 700, 400);
+//        info_area.setBackground(new Color(34, 41, 48));
+//        info_area.setForeground(Color.orange);
+//        info_area.setFont(new Font("SansSerif", Font.PLAIN, 18));
         
         panel.setBackground(new Color(34, 41, 48));
 
@@ -53,11 +55,12 @@ public class ShowCarGUI extends CarShower implements GUIImplementer{
         back_button.setForeground(new Color(34, 41, 48));
         back_button.setFont(new Font("SansSerif", Font.BOLD, 25));
 
-        panel.add(info_area);
+        panel.add(pane);
         panel.add(back_button);
         panel.add(show_cars);
-
+        
         showcar_frame.add(panel);
+//        showcar_frame.add(pane);
         showcar_frame.setSize(700, 600);
         showcar_frame.setVisible(true);
         showcar_frame.setResizable(false);
@@ -73,10 +76,12 @@ public class ShowCarGUI extends CarShower implements GUIImplementer{
     	
 //    	
         try{
+        	
 //        	Car = (Admin)objectInputStream.readObject();
             Scanner fes = new Scanner(file);
             while(fes.hasNextLine()){
-                info_area.append(fes.nextLine() + "\n---------------------------------------------------------------------------------------------------------------------------\n");
+            	str = fes.nextLine().split(" ");
+                info_area.append(str[1]+" "+ str[2]+"\n"+str[3]+" "+str[4]+"\n"+str[5]+" "+str[6]+"\n"+ str[7]+" "+str[8]+"\n"+ str[9]+" "+str[10]+"\n"+"\n--------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n");
             }
             fes.close();
 
